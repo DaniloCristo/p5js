b008774bd9b07ac23544cf9967a7b62b9e5c06b8
Danilo Marques <danilomarques20@hotmail.com>
2018-03-17 12:26:56 -0300

    Adicionar botão para gerar numero de lados aleatorios

--- formas_geometricas/main.js
+++ formas_geometricas/main.js
@@ -1,11 +1,13 @@
 let lados;
 let number;
+let c;
 function setup(){
 	createCanvas(600,400);
 	//array com os lados das formas
 	lados = [0,3,4];
 	//index aleatorio para o numero de lados do array acima
 	number = round(random(0,lados.length - 1));
+	c = color(255);
 }
 
 function draw(){
@@ -28,4 +30,31 @@ function draw(){
 	textSize(30);
 	//Exibindo o numero de lados
 	text("Numero de lados: "+lados[number],180,70);
+	botao();
+	//console.log(c);
+}
+
+//criação do botão
+let botao = function(){
+	fill(c);
+	rect(240,90,100,40);
+	fill(0);
+	textSize(14)
+	text("Clique em mim",240,110);
+	//mudar cor do botão caso pressionado em cima dele
+	if(mouseIsPressed && mouseX > 240 && mouseX < 340 && mouseY > 90 && mouseY < 130  ){
+			c = color(100,100,100);
+	}else{
+			c =  color(255,255,255);
+	}
+	
+}
+
+//quando clicar o botão dentro do rect do botão alterar o numero de lados
+mousePressed = function(){
+	if(mouseX > 240 && mouseX < 340 && mouseY > 90 && mouseY < 130 ){
+		number = round(random(0,lados.length - 1));
+	}
+
+
 }
